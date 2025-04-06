9d692f4455955d342df5d45bac39bc25c24e4840
Joao Vitor <jovitgomesou@gmail.com>
2025-04-06 12:59:30 -0300

    backend

--- api/Paciente.js
+++ api/Paciente.js
@@ -3,8 +3,9 @@ const Paciente = require('../models/Paciente');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
-// cadastro
-// http://localhost:3000/api/pacientes/cadastro
+// http://localhost:3000/api/pacientes/operacao 
+
+// create
 router.post('/cadastro', async (req, res) => {
     try {
       const { cpfPaciente, nomePaciente, dataNascimento, emailPaciente, senhaPaciente, telefonePaciente, enderecoPaciente, generoPaciente } = req.body;
@@ -23,7 +24,7 @@ router.post('/cadastro', async (req, res) => {
       });
   
       await novoPaciente.save();
-      res.status(201).json({ message: '[!] PACIENTE CADASTRADO COM SUCESSO' });
+      res.status(201).json({ message: '[!] Paciente cadastrado com sucesso.' });
     } catch (error) {
       console.error('[X] ERRO AO CADASTRAR PACIENTE', error.message);
       res.status(400).json({ error: error.message });
@@ -31,11 +32,61 @@ router.post('/cadastro', async (req, res) => {
 });
 
 // login
-router.post('/signin', (req, res) => {
-    
-})
+router.post('/login', async (req, res) => {
+  const { emailPaciente, senhaPaciente } = req.body;
 
-// deletar
+  try {
+    const paciente = await Paciente.findOne({ emailPaciente });
+
+    if (!paciente) {
+      return res.status(404).json({ error: '[?] PACIENTE NÃO ENCONTRADO.' });
+    }
+
+    const senhaCorreta = await bcrypt.compare(senhaPaciente, paciente.senhaPaciente);
+
+    if (!senhaCorreta) {
+      return res.status(401).json({ error: '[!] Senha inválida.' });
+    }
+
+    res.status(200).json({ message: 'Login realizado com sucesso', paciente });
+  } catch (error) {
+    res.status(500).json({ error: '[X] ERRO NO LOGIN ' + error.message });
+  }
+});
+
+// read e read por id
+router.get('/get', async (req, res) => {
+  try {
+    const pacientes = await Paciente.find();
+    res.status(200).json(pacientes);
+  } catch (error) {
+    res.status(500).json({ error: '[X] ERRO AO BUSCAR PACIENTE' }, error.message);
+  }
+});
+
+router.get('/get/:id', async (req, res) => {
+  try {
+    const paciente = await Paciente.findById(req.params.id);
+    if (!paciente) return res.status(404).json({ error: '[X] ERRO AO BUSCAR PACIENTE' }, error.message);
+    res.status(200).json(paciente);
+  } catch (error) {
+    res.status(400).json({ error: '[X] ID DE PACIENTE INVÁLIDO' });
+  }
+});
+
+
+// replace
+router.put('/atualizar/:id', async (req, res) => {
+  try {
+    const pacienteAtualizado = await Paciente.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!pacienteAtualizado) return res.status(404).json({ error: '[X] ERRO AO ATUALIZAR PACIENTE' }, error.message);
+    res.status(200).json(pacienteAtualizado);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// delete
 router.delete('/delete/:id', async (req, res) => {
     try {
         const { id } = req.params;
