e3dc0ab6468442b51b56ef1bb55e1ef60dcb823f
Joao Vitor <jovitgomesou@gmail.com>
2025-04-13 16:05:11 -0300

    feat: adiciona schemas e routers (post, get) para consultas

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const app = express();
 const port = 3000;
 
 const pacienteRouter = require('./api/Paciente');
+const consultaRouter = require('./api/Consulta');
 
 app.use(express.json());
 
+// routers
 app.use('/api/paciente', pacienteRouter);
+app.use('/api/consulta', consultaRouter)
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
