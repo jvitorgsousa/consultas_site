1c51aac5990944a9718e0f2b0a7553dd9cdef470
Joao Vitor <jovitgomesou@gmail.com>
2025-05-09 23:11:30 -0300

    feat: adiciona parte da documentação swagger da API do projeto (paciente, médico)

--- backend/api/Medico.js
+++ backend/api/Medico.js
@@ -2,6 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Medico = require('../models/Medico');
 
+/**
+ * @swagger
+ * /medico/registrar_medico:
+ *   post:
+ *     summary: Registra um novo médico
+ *     tags: [Medico]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nomeMedico:
+ *                 type: string
+ *               crmMedico:
+ *                 type: string
+ *               especialidade:
+ *                 type: string
+ *               data_disponibilidade:
+ *                 type: string
+ *                 pattern: "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$"
+ *               hora_disponibilidade:
+ *                 type: string
+ *                 pattern: "^([01]\\d|2[0-3]):([0-5]\\d)$"
+ *     responses:
+ *       201:
+ *         description: Médico registrado com sucesso
+ *       400:
+ *         description: Erro ao registrar médico
+ */
 router.post('/registrar_medico', async (req, res) => {
     try {
         const novoMedico = new Medico(req.body);
@@ -12,6 +43,25 @@ router.post('/registrar_medico', async (req, res) => {
       }
 });
 
+/**
+ * @swagger
+ * /medico/listar_medicos:
+ *   get:
+ *     summary: Lista todos os médicos cadastrados
+ *     tags: [Medico]
+ *     responses:
+ *       200:
+ *         description: Lista de médicos retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/models/Medico'
+ *       500:
+ *         description: Erro ao buscar médicos
+ */
+
 router.get('/listar_medicos', async (req, res) => {
     try {
       const medicos = await Medico.find();
@@ -20,7 +70,28 @@ router.get('/listar_medicos', async (req, res) => {
       res.status(500).json({ error: 'Erro ao buscar médicos', details: error.message });
     }
   });
-  
+
+/**
+ * @swagger
+ * /medico/deletar/{crm}:
+ *   delete:
+ *     summary: Deleta um médico pelo CRM
+ *     tags: [Medico]
+ *     parameters:
+ *       - in: path
+ *         name: crm
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: CRM do médico
+ *     responses:
+ *       200:
+ *         description: Médico deletado com sucesso
+ *       404:
+ *         description: Médico não encontrado
+ *       500:
+ *         description: Erro ao deletar médico
+ */
 router.delete('/deletar/:crm', async (req, res) => {
     try {
       const medicoDeletado = await Medico.findOneAndDelete({ crmMedico: req.params.crm });
